31719fb2fe546b712b0adfb393771b0e15aca01f
adongtt <240732222@qq.com>
2025-02-21 13:44:38 +0800

    Update site-metadata.ts

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -10,14 +10,14 @@ interface ISiteMetadataResult {
 }
 
 const data: ISiteMetadataResult = {
-  siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
-  logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
+  siteTitle: 'xiaobu Running Page',
+  siteUrl: '',
+  logo: 'https://dl.goeppgroup.com/assets/images/20250221133940.jpg',
   description: 'Personal site and blog',
   navLinks: [
     {
       name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      url: '',
     },
     {
       name: 'About',
